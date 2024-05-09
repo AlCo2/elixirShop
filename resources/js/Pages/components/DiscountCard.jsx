898aeee9cf69bef665809448c8d790a39206e095
Alco2 <abdou.lahboub.wwe@gmail.com>
2024-05-09 18:55:20 +0100

    improvement on cards

--- resources/js/Pages/components/DiscountCard.jsx
+++ resources/js/Pages/components/DiscountCard.jsx
@@ -1,3 +1,4 @@
+import { Link } from '@inertiajs/react';
 import { Box, Button, Card, CardContent, CardMedia, Grid, Typography} from '@mui/material';
 import React from 'react'
 import { FaShoppingBasket } from 'react-icons/fa';
@@ -5,17 +6,19 @@ import { FaShoppingBasket } from 'react-icons/fa';
 const DiscountCard = ({id, title, image, price, discountPrice}) => {
   return (
     <Grid item >
-      <Card sx={{width:240, ":hover":{boxShadow:5}, cursor:'pointer'}}>
-        <Box mr={1}>
-            <p className='font-Poppins text-red-500 text-right'>{'-'+parseInt(((price - discountPrice) / (price)) * 100)+'%'}</p>
+      <Card sx={{width:240, ":hover":{boxShadow:5}, cursor:'pointer', position:'relative'}}>
+        <Box>
+            <p className='absolute right-1 top-1 text-white font-Poppins bg-liliana-primary text-right px-2 py-1 rounded-lg'>{'-'+parseInt(((price - discountPrice) / (price)) * 100)+'%'}</p>
         </Box>
-        <Box display={'flex'} justifyContent={'center'}>
-          <CardMedia component={'img'}
-            sx={{height:200, width:200, borderRadius:5}}
-            image={image}
-            alt={title}
-          />
-        </Box>
+        <Link href={'/store/product/'+id}>
+          <Box display={'flex'} justifyContent={'center'}>
+              <CardMedia component={'img'}
+                sx={{height:180, width:'100%'}}
+                image={image}
+                alt={title}
+              />
+          </Box>
+        </Link>
         <Box display={'flex'} height={70} justifyContent={'center'}>
           <CardContent>
             <p className='text-center font-bold'>{title}</p>
@@ -28,7 +31,7 @@ const DiscountCard = ({id, title, image, price, discountPrice}) => {
                     <p className='font-Poppins p-2 text-white font-bold'>{discountPrice}.00DH</p>
                 </div>
             </Box>
-          <Button href={'/store/product/'+id} variant="contained" color='liliana_secondary'><FaShoppingBasket/></Button>
+          <Button href={'/store/product/'+id} variant="contained" color='liliana_third'><FaShoppingBasket/></Button>
         </Box>
       </Card>
     </Grid>
--- resources/js/Pages/components/IntroCard.jsx
+++ resources/js/Pages/components/IntroCard.jsx
@@ -1,26 +1,54 @@
-import { Box, Button, Card, CardContent, CardMedia, Grid, Typography} from '@mui/material';
+import { Link } from '@inertiajs/react';
+import { Alert, Box, Button, Card, CardContent, CardMedia, Grid, Snackbar, Typography} from '@mui/material';
+import axios from 'axios';
+import { useState } from 'react';
 import { FaShoppingBasket } from 'react-icons/fa';
 const IntroCard = ({id, title, image, price}) => {
+  const [open, setOpen] = useState(false);
+  function addToCart(e){
+    e.preventDefault();
+    const data = {
+        product_id:id
+    }
+    axios.post('/api/cart/add', data, {preserveScroll:true});
+    setOpen(true);
+  }
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setOpen(false);
+  };
   return (
     <Grid item >
+      <Snackbar
+      open={open}
+      autoHideDuration={5000}
+      onClose={handleClose}
+      >
+        <Alert onClose={handleClose} severity='success' variant='filled' sx={{width:'100%'}}>
+          {title} added to cart
+        </Alert>
+      </Snackbar>
       <Card sx={{width:240, ":hover":{boxShadow:5}, cursor:'pointer'}}>
-        <Box display={'flex'} justifyContent={'center'}>
-          <CardMedia component={'img'}
-            sx={{height:200, width:200, borderRadius:5}}
-            image={image}
-            alt={title}
-          />
-        </Box>
+        <Link href={'/store/product/'+id}>
+          <Box display={'flex'} justifyContent={'center'}>
+              <CardMedia component={'img'}
+                sx={{height:180, width:'100%'}}
+                image={image}
+                alt={title}
+              />
+          </Box>
+        </Link>
         <Box display={'flex'} height={70} justifyContent={'center'}>
-          <CardContent>              
-            <Typography textAlign={'center'} variant="h6">
-              {title}
-            </Typography>
+          <CardContent>
+            <p className='text-center font-bold'>{title}</p>
           </CardContent>
         </Box>
         <Box display={'flex'} margin={2} justifyContent={'space-between'} alignItems={'center'}>
           <Typography fontFamily={'Poppins'} variant="body2">{price}DH</Typography>
-          <Button href={'/store/product/'+id} variant="contained" color='liliana_secondary'><FaShoppingBasket/></Button>
+          <Button onClick={addToCart} variant="contained" color='liliana_third'>Add to cart</Button>
         </Box>
       </Card>
     </Grid>
