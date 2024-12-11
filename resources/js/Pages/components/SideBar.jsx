3f6d8e5ad5d3d38f68fd88eea946298e252dc188
Alco2 <abdou.lahboub.wwe@gmail.com>
2024-12-11 16:40:33 +0100

    update sidebar

--- resources/js/Pages/components/SideBar.jsx
+++ resources/js/Pages/components/SideBar.jsx
@@ -22,13 +22,19 @@ import { Link, usePage } from '@inertiajs/react';
 import { BiUser } from 'react-icons/bi';
 import { IconButton } from '@mui/material';
 
+
+const NavItem = ({title, path}) => {
+  const pathname = window.location.pathname
+  return (
+      <a className={`h-10 flex font-Roboto items-center pl-4 w-full duration-300 font-bold hover:bg-gray-100 ${pathname===path?'bg-gray-100 opacity-100':'bg-white opacity-50'}`} href={path}>{title}</a>
+  )
+}
 export default function SideBar() {
   const [open, setOpen] = useState(false);
   const { auth } = usePage().props;
   const toggleDrawer = (newOpen) => () => {
     setOpen(newOpen);
   };
-
   const DrawerList = (
     <Box sx={{ width: 250 }} role="presentation">
       <List>
@@ -38,49 +44,22 @@ export default function SideBar() {
             </IconButton>
           </ListItem>
           <ListItem>
-            <a href='/' className='flex items-center ml-2'>
-              <p className="text-xl font-bold font-Poppins ml-2 duration-300">Elixir</p>
-            </a>
+            <a href='/' className='flex items-center ml-2 text-xl font-bold font-Poppins duration-300'>Elixir</a>
           </ListItem>
           <ListItem disablePadding>
-            <ListItemButton href='/'>
-              <ListItemIcon>
-                <BsHouse/>
-              </ListItemIcon>
-              <ListItemText primary={"Home"} />
-            </ListItemButton>
+            <NavItem title={"Home"} path={'/'}/>
           </ListItem>
           <ListItem disablePadding>
-            <ListItemButton href='/store'>
-              <ListItemIcon>
-                <BsShop/>
-              </ListItemIcon>
-              <ListItemText primary={"Parfumes"} />
-            </ListItemButton>
+            <NavItem title={"Store"} path={'/store'}/>
           </ListItem>
           <ListItem disablePadding>
-            <ListItemButton href='/store/man'>
-              <ListItemIcon>
-              <IoManOutline/>
-              </ListItemIcon>
-              <ListItemText primary={"Man"} />
-            </ListItemButton>
+            <NavItem title={"Man"} path={'/store/man'}/>
           </ListItem>
           <ListItem disablePadding>
-            <ListItemButton href='/store/woman'>
-              <ListItemIcon>
-                <IoWomanOutline/>
-              </ListItemIcon>
-              <ListItemText primary={"Woman"} />
-            </ListItemButton>
+          <NavItem title={"Woman"} path={'/store/woman'}/>
           </ListItem>
           <ListItem disablePadding>
-            <ListItemButton href='/support'>
-              <ListItemIcon>
-                <BsTelephone/>
-              </ListItemIcon>
-              <ListItemText primary={"Support"} />
-            </ListItemButton>
+            <NavItem title={"Support"} path={'/support'}/>
           </ListItem>
           {auth.user && auth.user.role_id==1?
           <ListItem disablePadding>
